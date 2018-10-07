4ddf842236d43760d14077f9d2b4a23acee733c7
jglrxavpok <jglrxavpok@gmail.com>
2018-10-07 12:06:46 +0200

    Make sure players have waypoint data on servers + changed rope rendering

--- src/main/kotlin/org/jglrxavpok/moarboats/server/Proxy.kt
+++ src/main/kotlin/org/jglrxavpok/moarboats/server/Proxy.kt
@@ -1,7 +1,38 @@
 package org.jglrxavpok.moarboats.server
 
+import net.minecraft.entity.player.EntityPlayerMP
+import net.minecraft.item.ItemMapBase
+import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.event.entity.living.LivingEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.gameevent.PlayerEvent
+import org.jglrxavpok.moarboats.MoarBoats
 import org.jglrxavpok.moarboats.common.MoarBoatsProxy
+import org.jglrxavpok.moarboats.common.items.ItemGoldenTicket
+import org.jglrxavpok.moarboats.common.network.S21SetGoldenItinerary
 
 class Proxy: MoarBoatsProxy() {
 
+
+    override fun preInit() {
+        super.preInit()
+        MinecraftForge.EVENT_BUS.register(this)
+    }
+
+    @SubscribeEvent
+    fun onPlayerUpdate(event: LivingEvent.LivingUpdateEvent) {
+        val player = event.entityLiving as? EntityPlayerMP ?: return
+        for (i in 0 until player.inventory.sizeInventory) {
+            val itemstack = player.inventory.getStackInSlot(i)
+
+            if (!itemstack.isEmpty && itemstack.item == ItemGoldenTicket) {
+                if(!ItemGoldenTicket.isEmpty(itemstack)) {
+                    if(player.ticksExisted % 5 == 0) { // send every 5 ticks
+                        val data = ItemGoldenTicket.getData(itemstack)
+                        MoarBoats.network.sendTo(S21SetGoldenItinerary(data), player)
+                    }
+                }
+            }
+        }
+    }
 }
